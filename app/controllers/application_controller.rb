ddf99db0aa4d78e837f9f4c7292abd7cfba67ad0
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-30 15:34:22 -0600

    commit tree 09bf284a614e912f3c0aad66416e21763351e56b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect "/articles"
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do 
+    @article = Article.new
+    erb :new
+  end
+
+  post "/articles" do 
+    @article = Article.create(title: "#{params[:title]}", content: "#{params[:content]}")
+    redirect  "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+
+    redirect "/articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect "/articles"
+  end
+
+
+ 
+
 end
